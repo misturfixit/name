82dc8a4fce5415060ac61e048de4cc0edb3ea946
misturfixit <japullins420@gmail.com>
2017-06-27 14:03:23 -0400

    name1.rb

--- name1.rb
+++ name1.rb
@@ -1,17 +1,51 @@
-def F_name(first)
-	return "Mi first name is " + (first)
+def get_firstname()
+
+	print "What's your first name ?: "
+	f_name = gets.chomp 
 end
 
-def L_name(last)
-	return "Mi last name is " + (last)
+def get_lastname()
+
+	print "What's your last name ?: "
+	l_name = gets.chomp
 end
 
 
+def print_name 
+	
+	f_name = get_firstname()
+	l_name = get_lastname()
+   
+    puts "Your name is     #{f_name} #{l_name}??? 
+          
+          DOES NOT COMPUTE !!!
+
+         
+         TERMINATION EMINENT!!!!
+        
+
+        SELF-DESTRUCT IN .... 
 
 
 
+               5...                             
+              .                                   
+       	     .                         
+       	    4...                              
+           .
+     	  .
+     	 3...                               
+        .
+       .
+     2...                                 
+      .
+      . 
+     .
+     ...BWAHAHAHAHAHAHAHAHA!!!!
+
+    gotya...;)"
+
+end
 
 
-p F_name("Joe")
-p L_name("Pullins") 
-puts
+puts print_name
